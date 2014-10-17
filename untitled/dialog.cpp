d3503c7ba25da12d4d73e16098ec9cb1024683b7
zengxx_lab_ubuntu <shylonezeng@gmail.com>
2014-10-17 22:32:28 +0800

    add files to empty repo

---
+++ untitled/dialog.cpp
@@ -0,0 +1,170 @@
+/****************************************************************************
+**
+** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** You may use this file under the terms of the BSD license as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of Digia Plc and its Subsidiary(-ies) nor the names
+**     of its contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QtWidgets>
+
+#include "dialog.h"
+
+//! [0]
+Dialog::Dialog()
+{
+    createMenu();
+    createleftView();
+    createrightView();
+//    createFormGroupBox();
+//! [0]
+
+//! [1]
+    bigEditor = new QTextEdit;
+    bigEditor->setPlainText(tr("This widget takes up all the remaining space "
+                               "in the top-level layout."));
+
+    buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok
+                                     | QDialogButtonBox::Cancel);
+
+    connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+    connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+//! [1]
+
+//! [2]
+    QHBoxLayout *mainLayout = new QHBoxLayout;
+//! [2] //! [3]
+    mainLayout->setMenuBar(menuBar);
+//! [3] //! [4]
+ //   mainLayout->addWidget(leftView);
+    mainLayout->addWidget(rightView);
+  //  mainLayout->addWidget(formGroupBox);
+  //  mainLayout->addWidget(bigEditor);
+//    mainLayout->addWidget(buttonBox);
+//! [4] //! [5]
+//!
+//!
+    setLayout(mainLayout);
+
+    setWindowTitle(tr("Basic Layouts"));
+}
+//! [5]
+
+//! [6]
+void Dialog::createMenu()
+{
+    menuBar = new QMenuBar;
+
+    fileMenu = new QMenu(tr("&File"), this);
+    exitAction = fileMenu->addAction(tr("E&xit"));
+    menuBar->addMenu(fileMenu);
+
+    connect(exitAction, SIGNAL(triggered()), this, SLOT(accept()));
+}
+//! [6]
+
+//! [7]
+void Dialog::createleftView()
+{
+    leftView = new QGroupBox(tr("H!!!!orizontal layout"));
+    QVBoxLayout *layout = new QVBoxLayout;
+    createlefttopView();
+    createleftbelowView();
+    layout->addWidget(lefttopView);
+    layout->addWidget(leftbelowView);
+    /*
+    for (int i = 0; i < NumButtons; ++i) {
+        buttons[i] = new QPushButton(tr("Button %1").arg(i + 1));
+	layout->addWidget(buttons[i]);
+    }
+    */
+    leftView->setLayout(layout);
+}
+void Dialog::createlefttopView()
+{
+    lefttopView=new QGroupBox(tr("left top view"));
+
+}
+void Dialog::createleftbelowView()
+{
+    leftbelowView=new QGroupBox(tr("left right view"));
+}
+//! [7]
+
+//! [8]
+void Dialog::createrightView()
+{
+    rightView = new QGroupBox(tr("Grid layout"));
+//! [8]
+    QVBoxLayout *layout = new QVBoxLayout;
+
+//! [9]
+/*    for (int i = 0; i < NumGridRows; ++i) {
+        labels[i] = new QLabel(tr("Line %1:").arg(i + 1));
+        lineEdits[i] = new QLineEdit;
+        layout->addWidget(labels[i], i + 1, 0);
+        layout->addWidget(lineEdits[i], i + 1, 1);
+    }
+
+//! [9] //! [10]
+    smallEditor = new QTextEdit;
+    smallEditor->setPlainText(tr("This widget takes up about two thirds of the "
+                                 "grid layout."));
+    layout->addWidget(smallEditor, 0, 2, 4, 1);
+//! [10]
+
+//! [11]
+    layout->setColumnStretch(1, 10);
+    layout->setColumnStretch(2, 20);
+    */
+    rightView->setLayout(layout);
+
+}
+
+//! [11]
+
+//! [12]
+/*
+void Dialog::createFormGroupBox()
+{
+    formGroupBox = new QGroupBox(tr("Form layout"));
+    QFormLayout *layout = new QFormLayout;
+    layout->addRow(new QLabel(tr("Line 1:")), new QLineEdit);
+    layout->addRow(new QLabel(tr("Line 2, long text:")), new QComboBox);
+    layout->addRow(new QLabel(tr("Line 3:")), new QSpinBox);
+    formGroupBox->setLayout(layout);
+}
+*/
+//! [12]
---
+++ untitled/untitled.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-10-17T21:51:04
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = untitled
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        dialog.cpp
+
+
+HEADERS  += dialog.h
+
