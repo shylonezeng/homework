1c1a632818a95c79e99e4e1448a369020e485085
shylonezeng <shylonezeng@gmail.com>
2014-12-07 11:59:39 +0800

    travel_problem: coding for core algorithm

--- travel_problem/graph_m.cpp
+++ travel_problem/graph_m.cpp
@@ -11,11 +11,12 @@
 using namespace std;
 
 class CGraph{
-	private:
+	public:
 		int node_num; //number  of graph nodes
 		int arc_num;	//number of graph arc
-		int **mt; //matrix;
-		bool ** visit_flag;
+		int **mt;	// use two halt triangle part to store arc infomation
+	private:
+		bool * visit_flag;
 		void destroy();
 	public:
 		CGraph();
@@ -27,8 +28,9 @@ class CGraph{
 		void addArc(int snode,int enode,int value);
 		void addArc(int snode,int enode){addArc(snode,enode,1);}
 		//void getNode(int row,int col);
-		bool ifVisited(int row,int col);
-		void visit(int row ,int col);
+		bool ifVisited(int index);
+		void visit(int index);
+		bool setVisitFlag(int index,int visit_time);
 		void setNode(int node_num);
 		void setArc(int **arcs);
 		void show();
@@ -61,42 +63,51 @@ void CGraph::setNode(int node_num)
 	}
 	this->node_num=node_num;
 	mt=new int*[this->node_num];
-	visit_flag =new bool*[this->node_num];
+	visit_flag =new bool[this->node_num];
 	for(int i=0;i<this->node_num;++i){
 
 		mt[i]=new int[this->node_num];
-		visit_flag[i]=new bool[this->node_num];
+// 		visit_flag[i]=new bool[this->node_num];
 	}
+
 	memset(mt,sizeof(int)*node_num*node_num,0);
 	memset(visit_flag,sizeof(bool)*node_num*node_num,0);
 }
 void CGraph::addArc(int snode,int enode,int value){
 	mt[snode][enode]=value;
+	mt[enode][enode]=value;
 }
 
-bool CGraph::ifVisited(int row,int col)
+bool CGraph::ifVisited(int index)
 {
 
-	if(visit_flag[row][col])
+	if(visit_flag[index])
 		return true;
 	return false;
 }
-void CGraph::visit(int row , int col )
+void CGraph::visit(int index )
 {
 	//do something
 	//...
-	visit_flag[row][col]=true;
+	visit_flag[index]=true;
 }
+bool CGraph::setVisitFlag(int index ,int visit_time){
+	if(index>=node_num)
+		return false;
+	this->visit_flag[node_num]=visit_time;
+
+}
+
 /*useless function*/
 // void CGraph::addNode(int node_id){
-// 	cout<<"fail in adding node"<<endl;f
+// 	cout<<"fail in adding node"<<endl;
 // }
 
 void CGraph::show ()
 {
 	for(int i=0;i<node_num; ++i){
 		for(int j=0;j<node_num; ++j)
-			cout<<mt[i][j]<<"["<<visit_flag[i][j]<<"]\t";
+			cout<<mt[i][j]<<" ";
 		cout<<endl;
 	}
 
@@ -104,12 +115,13 @@ void CGraph::show ()
 /*TO DO*/
 void CGraph::setArc(int **arcs){
 }
+
 void CGraph::destroy(){
 	for(int i=0;i<node_num;++i){
 		if(mt[i])
 			delete mt[i];
-		if(visit_flag[i])
-			delete visit_flag[i];
+// 		if(visit_flag[i])
+// 			delete visit_flag[i];
 	}
 // 	if(mt)
 // 		delete mt;
@@ -125,12 +137,46 @@ void printArray(MylistType list){
 	cout<<endl;
 }
 /*search path function*/
-vector<int>& search_path(CGraph g){//maybe some erro can happen when it returns a reference type
+vector<int>& search_path(CGraph &g){//maybe some erro can happen when it returns a reference type
  	#ifdef debug
 		cout<<"call function  search_path"<<endl;
 	#endif
-	
-
+	/*start node can be visited 2 times*/
+	g.setVisitFlag(0,2);
+	int max_passby=node_num-1;
+	//each row index means number of nodes passed from source node to destination node
+	//min number start with index 1;
+	//log element logs next visit node 
+	int **log = new int*[max_passby+1]; 
+	log[0]=NULL;
+	/*nodes passed by can be 1 to max_passby*/
+	//when number of nodes passby is one;
+	int psby=1;
+	for(int p_node=1;p_node<node_num;p_node++){
+		log[psby]=new int[node_num];
+		memset(log[psby],0,sizeof(int)*node_num);
+		
+		if(g.mt[0][p_node])
+			log[psby][p_node]=1;
+	}
+/*没有考虑每个节点只能访问一次的问题*/
+	for(psby=1;psby<=max_passby;++psby){
+		log[psby]=new int[node_num];
+		memset(log[psby],0,sizeof(int)*node_num);
+		
+		for(int p_node=1;p_node<node_num;p_node++){
+			//find Adjacent node and evalue weather can pass by it 
+			//can not be involed in visitd node set
+			for(int adj=1;adj<node_num;adj++){
+				if(is adjacent node ||  if involed in visited path){
+					if(!canReach(adjnode))
+					
+				}
+			}
+		}	
+	}
+	/*find longest path*/
+		
 }
 
 
