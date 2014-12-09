1295c4ae29d97635746ce0115df80e7f5e22a032
zengxx_win7 <1510511289@qq.com>
2014-12-09 13:55:35 +0800

    correct to lastest version

--- travel_problem/graph_m.cpp
+++ travel_problem/graph_m.cpp
@@ -9,7 +9,7 @@
 #include <utility>
 
 #ifndef debug
-#define debug
+	#define debug
 #endif
 #define ivector vector<int>
 // typedef vector<int> ivector;
@@ -30,6 +30,7 @@ class CGraph{
 		bool * visit_flag;
 		typedef vector<vector<int> >  adjNodes_type;
 		adjNodes_type adjNodes;
+		adjNodes_type back_adjNodes;
 		int p_adj;
 		void destroy();
 	public:
@@ -51,7 +52,7 @@ class CGraph{
 
 		void setNextNodes();
 		void resetNextNodes();
-		int nextNode(int , int *);
+		int nextNode(int , int *,bool dir);
 		int getWestest(){ return 0;}
 		int getEastest(){ return node_num-1;}
 };
@@ -64,7 +65,7 @@ CGraph::~CGraph(){
 CGraph::CGraph()
 {
 	node_num=0;
-	arc_num=0;	
+	arc_num=0;
 	setNode(0);
 }
 CGraph::CGraph(int node_num)
@@ -81,7 +82,7 @@ void CGraph::setNode(int node_num)
 	{
 		mt=NULL;
 		visit_flag=NULL;
-		return ;	
+		return ;
 	}
 	this->node_num=node_num;
 	mt=new int*[this->node_num];
@@ -97,7 +98,7 @@ void CGraph::setNode(int node_num)
 }
 void CGraph::addArc(int snode,int enode,int value){
 	mt[snode][enode]=value;
-	mt[enode][enode]=value;
+	mt[enode][snode]=value;
 }
 
 bool CGraph::ifVisited(int index)
@@ -158,27 +159,32 @@ void CGraph::setNextNodes(){
 
 			if(!mt[i][j])
 				continue;
-			oneNode_adj.push_back(j);	
+			oneNode_adj.push_back(j);
 		}
 		this->adjNodes.push_back(oneNode_adj);
-	}		
+	}
 }
 void CGraph::resetNextNodes(){
 	for(int i=0;i<node_num;++i)
 	{
-		vector<int>& oneNode_adj=this->adjNodes[i];
-		oneNode_adj.clear();
-
+		vector<int> oneNode_adj;
 		for(int j=i-1;j>=0;--j){
 
 			if(!mt[i][j] )
 				continue;
-			oneNode_adj.push_back(j);	
+			oneNode_adj.push_back(j);
 		}
-	}		
+		this->back_adjNodes.push_back(oneNode_adj);
+	}
 }
-int CGraph::nextNode(int cur_node, int *which_one){
-	vector<int>& oneNode_adj=this->adjNodes[cur_node];	
+int CGraph::nextNode(int cur_node, int *which_one,bool dir){
+	vector<int> oneNode_adj;
+	//catch exception
+	if(!dir)
+		oneNode_adj=this->adjNodes[cur_node];
+	else
+		oneNode_adj=this->back_adjNodes[cur_node];
+
 	if((*which_one)>=oneNode_adj.size())
 		return -1;
 	return oneNode_adj[(*which_one)++];
@@ -187,7 +193,7 @@ int CGraph::nextNode(int cur_node, int *which_one){
 template <typename MylistType>
 void printArray(MylistType list){
 	typename MylistType::iterator iter=list.begin();
-	while(iter!=list.end())
+	while((iter++)!=list.end())
 		cout<<*iter<<" ";
 	cout<<endl;
 }
@@ -211,54 +217,69 @@ typedef vector<int> value_of_path;
 typedef map<key_type,value_of_path> Log_t;
 // bool back_path=false;
 
-bool recurse(CGraph &g,Log_t &log,vector<int> visited_flag,vector<int> &path,int cur_node){
+bool recurse(CGraph &g,Log_t &log,vector<int> visited_flag,vector<int> &path,int cur_node,bool dir){
 	//
 
 	// 	if(back_path){
 	// 		g.resetNextNodes();
 	// 		back_path=false;
 	// 	}
-	//
+	//sf
+
+	//cout<<"visiting node:"<<cur_node<<endl;
+	// system("paus");
+
+			cout<<"tempPath:"<<endl;
+			printArray<ivector>(path);
+
+
 	path.push_back(cur_node);
 	visited_flag[cur_node]=1;
-	//end recursion condition	
+	//end recursion condition
 	if(cur_node==g.getWestest()){
 		return true;
+	}
+	if(cur_node==g.getEastest()){
+		dir=1;
+		//g.resetNextNodes();
 	}
 
 	//if loged then return;
 	key_type key=key_type(visited_flag,cur_node);
 	Log_t::iterator iter=log.find(key);
 	if(iter!=log.end() ){
+		cout<<"loged,need not continue"<<endl;
 		vcopy(path,log[key]);
 		return false;
-	}		
+	}
 	// the best path
 	int maxPsbyNodes=0;
 	vector<int> bestPath;
 	//
 	int which_adj=0;
-	int nNode=g.nextNode(cur_node,&which_adj);
+
+	int nNode=g.nextNode(cur_node,&which_adj,dir);
 
 	while(nNode!=-1){
-		vector<int> tempPath(path);	//copy of path. !!! may deallocted before referred
-		recurse(g,log,visited_flag,tempPath,nNode);
-		int len=tempPath.size();
-		if(len<=maxPsbyNodes){
-			maxPsbyNodes=len;
-			bestPath=tempPath;
-		}
+		if(!visited_flag[nNode]){
+			vector<int> tempPath(path);	//copy of path. !!! may deallocted before referred
+			recurse(g,log,visited_flag,tempPath,nNode,dir);
+			int len=tempPath.size();
+			if(len<=maxPsbyNodes){
+				maxPsbyNodes=len;
+				bestPath=tempPath;
+			}
 
-		nNode=g.nextNode(cur_node,&which_adj);
+		}
+		nNode=g.nextNode(cur_node,&which_adj,dir);
 	}
+// 	cout<<"bestPath:"<<endl;
+// 	printArray<ivector>(bestPath);
 	//deep copy the best
 	path=bestPath;
 	//if not loged, then log;
-	log[key]=path;	
+	log[key]=path;
 
-	if(cur_node==g.getEastest()){
-		g.resetNextNodes();
-	}
 	return false; //not returned  back
 }
 /*search path function*/
@@ -274,7 +295,7 @@ bool search_path(CGraph &g, ivector& bestPath){//maybe some erro can happen when
 	// 	int max_passby=node_num-1;
 	//each row index means number of nodes passed from source node to destination node
 	//min number start with index 1;
-	//log element logs next visit node 
+	//log element logs next visit node
 	// 	int log_length=(int)pow(2,n);
 	Log_t log;
 	vector<int> visited_flag(node_num,0);
@@ -283,18 +304,22 @@ bool search_path(CGraph &g, ivector& bestPath){//maybe some erro can happen when
 // 	vector<int> bestPath;
 
 	g.setNextNodes();
+	g.resetNextNodes();
 
+	bool direction=0;
 	int which_one=0;
-	int nNode=g.nextNode(0,&which_one);
+	int nNode=g.nextNode(0,&which_one,direction);
+
 	while(nNode!=-1){
 		vector<int> path;
-		recurse(g,log,visited_flag,path,nNode);
+		recurse(g,log,visited_flag,path,nNode,direction);
+
 		int len=path.size();
 		if(len>maxPsbyNodes){
 			maxPsbyNodes=len;
 			bestPath=path;
 		}
-		nNode=g.nextNode(0,&which_one);
+		nNode=g.nextNode(0,&which_one,direction);
 	}
 	// be sure to print out all elements
 	return true;
@@ -309,7 +334,7 @@ bool search_path(CGraph &g, ivector& bestPath){//maybe some erro can happen when
 // 	for(int p_node=1;p_node<node_num;p_node++){
 // 		log[psby]=new int[node_num];
 // 		memset(log[psby],0,sizeof(int)*node_num);
-// 		
+//
 // 		if(g.mt[0][p_node])
 // 			log[psby][p_node]=1;
 // 	}
@@ -317,24 +342,24 @@ bool search_path(CGraph &g, ivector& bestPath){//maybe some erro can happen when
 // 	for(psby=1;psby<=max_passby;++psby){
 // 		log[psby]=new int[node_num];
 // 		memset(log[psby],0,sizeof(int)*node_num);
-// 		
+//
 // 		for(int p_node=1;p_node<node_num;p_node++){
-// 			//find Adjacent node and evalue weather can pass by it 
+// 			//find Adjacent node and evalue weather can pass by it
 // 			//can not be involed in visitd node set
 // 			for(int adj=1;adj<node_num;adj++){
 // 				if(is adjacent node ||  if involed in visited path){
 // 					if(!canReach(adjnode))
-// 					
+//
 // 				}
 // 			}
-// 		}	
+// 		}
 // 	}
 // 	/*find longest path*/
-// 		
+//
 // }
 
 
-/*main function*/ 
+/*main function*/
 int main(int argc,char** argv)
 {
 #ifdef debug
@@ -351,13 +376,23 @@ int main(int argc,char** argv)
 			int arc_value=0;
 			int snode,enode;
 			cin>>snode>>enode;
-			g.addArc(snode,enode);	
+			g.addArc(snode,enode);
 		}
 		g.show();
 		/*search the travel path*/
-		 search_path(g,path);
+		search_path(g,path);
 	}
+	path.clear();
+
+	// // path.push_back(0);
+	// path.push_back(1);
+	// path.push_back(2);
+	// path.push_back(3);
+
 	printArray<ivector>(path);
+
+
+	cout<<"path size:"<<path.size()<<"\t end functoin main. "<<endl;
 	return 0;
 	/*reallocate new resource*/
 	//descontrutor function do complete this task
